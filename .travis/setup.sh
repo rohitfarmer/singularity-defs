3d8330b445c3d67a5a6bb9a96502874dcc5c37c3
Rohit Farmer <rohit.farmer@gmail.com>
2019-04-18 15:22:43 -0400

    Push container to google drive

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -14,6 +14,16 @@ sudo apt-get -qq update && sudo apt-get -qq install -y wget git \
 
 sudo sed -i -e 's/^Defaults\tsecure_path.*$//' /etc/sudoers
 
+# Install Singularity
+
+cd /tmp && \
+    git clone -b vault/release-2.4 https://www.github.com/sylabs/singularity.git
+    cd singularity
+    ./autogen.sh
+    ./configure --prefix=/usr/local 
+    make --silent
+    sudo make --silent install
+
 # Check Python
 
 echo "Python Version:"
@@ -22,12 +32,3 @@ pip -q install sregistry[all]
 sregistry version
 
 echo "sregistry Version:"
-
-# Install Singularity
-
-cd /tmp && \
-    git clone -b vault/release-2.4 https://www.github.com/sylabs/singularity.git
-    cd singularity && \
-    ./autogen.sh > /dev/null && \
-    ./configure --prefix=/usr/local > /dev/null && \
-    make > /dev/null && sudo make install > /dev/null
